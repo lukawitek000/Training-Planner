bc06b2bce0bbbd8e4f89727718168db56292da5c
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-09-09 18:55:25 +0200

    Improve training list item

--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/TrainingsScreen.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/TrainingsScreen.kt
@@ -3,6 +3,7 @@ package com.lukasz.witkowski.training.planner.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -11,8 +12,10 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,11 +62,22 @@ fun TrainingListItemContent(
         modifier = modifier
             .padding(8.dp)
             .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
             .background(Color.LightGray)
             .padding(8.dp),
     ){
-        Row {
-            Icon(imageVector = Icons.Filled.Person, contentDescription = "Placeholder")
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.DarkGray)
+                    .padding(8.dp),
+                imageVector = Icons.Filled.Person,
+                contentDescription = "${training.name} image"
+            )
+            Spacer(modifier = Modifier.width(16.dp))
             Column {
                 Text(
                     text = training.name,
