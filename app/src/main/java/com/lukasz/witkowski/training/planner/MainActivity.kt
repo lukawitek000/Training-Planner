538e14264b20536f9c359a10591afc8d348a30e5
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-09-09 17:24:03 +0200

    bottom navigation bar

--- app/src/main/java/com/lukasz/witkowski/training/planner/MainActivity.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/MainActivity.kt
@@ -3,11 +3,26 @@ package com.lukasz.witkowski.training.planner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.lukasz.witkowski.training.planner.ui.CalendarScreen
+import com.lukasz.witkowski.training.planner.ui.ExercisesScreen
+import com.lukasz.witkowski.training.planner.ui.StatisticsScreen
+import com.lukasz.witkowski.training.planner.ui.TrainingsScreen
 import com.lukasz.witkowski.training.planner.ui.theme.TrainingPlannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +30,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TrainingPlannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            })
+                    }
+                ) {
+                    Navigation(navController = navController)
                 }
             }
         }
@@ -25,14 +48,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = BottomNavItem.Trainings.route) {
+        composable(BottomNavItem.Trainings.route){
+            TrainingsScreen()
+        }
+        composable(BottomNavItem.Exercises.route){
+            ExercisesScreen()
+        }
+        composable(BottomNavItem.Calendar.route){
+            CalendarScreen()
+        }
+        composable(BottomNavItem.Statistics.route){
+            StatisticsScreen()
+        }
+    }
 }
+
+@Composable
+fun BottomNavigationBar(
+    modifier: Modifier = Modifier,
+    items: List<BottomNavItem> = BottomNavItem.Items.list,
+    navController: NavController,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation (
+        modifier = modifier,
+        elevation = 5.dp
+            ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Yellow,
+                unselectedContentColor = Color.LightGray,
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(imageVector = item.icon, contentDescription = item.title)
+                        Text(
+                            text = item.title,
+                            textAlign =  TextAlign.Center
+                        )
+                    }
+                }
+            )
+        }
+
+    }
+
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TrainingPlannerTheme {
-        Greeting("Android")
+
     }
 }
