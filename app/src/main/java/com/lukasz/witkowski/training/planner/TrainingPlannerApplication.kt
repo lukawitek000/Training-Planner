d4a221b2c80dd26b03400ec89535423373f21781
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2021-09-11 19:57:24 +0200

    add Timber for logging

--- app/src/main/java/com/lukasz/witkowski/training/planner/TrainingPlannerApplication.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/TrainingPlannerApplication.kt
@@ -3,11 +3,15 @@ package com.lukasz.witkowski.training.planner
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class TrainingPlannerApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
