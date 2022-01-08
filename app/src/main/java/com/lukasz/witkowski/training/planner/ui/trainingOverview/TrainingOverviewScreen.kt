40736f852f7c6601da12cd08ed2e2ae555d6135d
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-08 14:15:37 +0100

    display description if it exists

--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewScreen.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewScreen.kt
@@ -1,11 +1,17 @@
 package com.lukasz.witkowski.training.planner.ui.trainingOverview
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lukasz.witkowski.shared.models.Training
 
 @Composable
 fun TrainingOverviewScreen(
@@ -14,21 +20,37 @@ fun TrainingOverviewScreen(
     navigateBack: () -> Unit
 ) {
     Scaffold(modifier = modifier) {
-        TrainingOverviewContent(modifier = Modifier)
+        TrainingOverviewContent(
+            modifier = Modifier,
+            training = viewModel.training
+        )
 
     }
 }
 
 @Composable
-fun TrainingOverviewContent(modifier: Modifier = Modifier) {
-    Column() {
-        Text(text = "Training overview")
+fun TrainingOverviewContent(
+    modifier: Modifier = Modifier,
+    training: Training
+) {
+    Column(modifier = modifier
+        .fillMaxHeight().padding(8.dp)// TODO here will be used inner padding
+        .background(Color.Green)
+    ) {
+        if(training.description.isNotEmpty()) {
+            Text(text = training.description, color = Color.Red)
+        }
     }
 }
 
 @Preview
 @Composable
 fun TrainingOverviewContentPreview() {
-    TrainingOverviewContent()
+    TrainingOverviewContent(
+        training = Training(
+            title = "Training title",
+            description = "Training description can be long aisuhlkjbfl dsfh lkj ;foasihfl kjhlfkjh lakjfhalskfhlk jgdfl jkgdfkjhsd"
+        )
+    )
 }
 
--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewViewModel.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewViewModel.kt
@@ -2,6 +2,7 @@ package com.lukasz.witkowski.training.planner.ui.trainingOverview
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.lukasz.witkowski.shared.models.Training
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,5 +12,9 @@ class TrainingOverviewViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    val training = Training(
+        title = "Training title",
+        description = "Training description can be long aisuhlkjbfl dsfh lkj ;foasihfl kjhlfkjh lakjfhalskfhlk jgdfl jkgdfkjhsd"
+    )
 
 }
