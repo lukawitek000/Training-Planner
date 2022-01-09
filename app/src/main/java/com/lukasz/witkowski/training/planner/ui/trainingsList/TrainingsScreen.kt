7382623cb7dd6ac8556845e8d21f5264ed4d9ec4
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-09 15:52:46 +0100

    Prepare training item layout

--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingsList/TrainingsScreen.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingsList/TrainingsScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -27,9 +31,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.lukasz.witkowski.shared.models.Category
 import com.lukasz.witkowski.shared.models.Training
 import com.lukasz.witkowski.shared.models.TrainingWithExercises
 import com.lukasz.witkowski.shared.utils.categoriesWithoutNone
+import com.lukasz.witkowski.training.planner.ui.components.CategoryChip
 import com.lukasz.witkowski.training.planner.ui.components.ListCardItem
 import com.lukasz.witkowski.training.planner.ui.components.NoDataMessage
 import com.lukasz.witkowski.training.planner.ui.exercisesList.CategoryFilters
@@ -86,6 +92,7 @@ fun TrainingListItemContent(
     modifier: Modifier = Modifier,
     trainingWithExercises: TrainingWithExercises
 ) {
+    val categories = trainingWithExercises.exercises.map { it.exercise.category }.filter { it != Category.None }
     Row(
         modifier = modifier,
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -98,18 +105,26 @@ fun TrainingListItemContent(
         ) {
             Text(
                 text = trainingWithExercises.training.title,
-                fontSize = 24.sp
+                fontSize = 28.sp
             )
-            Text(
-                text = trainingWithExercises.training.description,
-                fontSize = 14.sp
-            )
+            if(categories.isNotEmpty()){
+                Spacer(modifier = Modifier.height(16.dp))
+                LazyRow {
+                    items(categories) { item: Category ->
+                        CategoryChip(
+                            modifier = Modifier.padding(end = 8.dp),
+                            text = item.name,
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
         }
         Icon(
             modifier = Modifier.size(40.dp),
             imageVector = Icons.Filled.PlayArrow,
             contentDescription = "Start training",
-            tint = Color.Yellow,
+            tint = MaterialTheme.colors.primary,
         )
     }
 }
