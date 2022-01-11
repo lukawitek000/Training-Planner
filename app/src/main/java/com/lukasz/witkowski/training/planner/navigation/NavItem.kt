f0383529fe6f31f8ee2e8844f80668d203139380
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-11 19:37:53 +0100

    Set icons for trainings and exercises

--- app/src/main/java/com/lukasz/witkowski/training/planner/navigation/NavItem.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/navigation/NavItem.kt
@@ -5,18 +5,22 @@ import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Send
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material.icons.outlined.Star
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import com.lukasz.witkowski.training.planner.R
 
 sealed class NavItem(
     val route: String,
-    val icon: ImageVector?,
+    val icon: Int?,
     val title: String,
     val isBackArrow: Boolean = false
 ) {
-    object Trainings : NavItem("trainings", Icons.Outlined.ShoppingCart, "Trainings")
-    object Exercises : NavItem("exercises", Icons.Outlined.Send, "Exercises")
-    object Calendar : NavItem("calendar", Icons.Outlined.DateRange, "Calendar")
-    object Statistics : NavItem("statistics", Icons.Outlined.Star, "Statistics")
+
+    object Trainings : NavItem("trainings", R.drawable.trainings_icon, "Trainings")
+    object Exercises : NavItem("exercises", R.drawable.exercises_icon, "Exercises")
+    object Calendar : NavItem("calendar", R.drawable.exercises_icon, "Calendar")
+    object Statistics : NavItem("statistics", R.drawable.exercises_icon, "Statistics")
 
     object BottomNavItems {
         val list = listOf(
