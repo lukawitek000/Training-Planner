62f0c221a157414215264eb1a76114457297f8cc
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-14 14:15:40 +0100

    Display heart rate plot

--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewScreen.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/trainingOverview/TrainingOverviewScreen.kt
@@ -1,47 +1,29 @@
 package com.lukasz.witkowski.training.planner.ui.trainingOverview
 
-import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.absoluteOffset
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDownward
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.outlined.ArrowDropDown
-import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -50,32 +32,30 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.github.tehras.charts.line.LineChart
-import com.github.tehras.charts.line.LineChartData
 import com.lukasz.witkowski.shared.models.Category
 import com.lukasz.witkowski.shared.models.Exercise
 import com.lukasz.witkowski.shared.models.Training
 import com.lukasz.witkowski.shared.models.TrainingExercise
 import com.lukasz.witkowski.shared.models.TrainingWithExercises
 import com.lukasz.witkowski.shared.models.statistics.GeneralStatistics
-import com.lukasz.witkowski.shared.utils.ResultHandler
 import com.lukasz.witkowski.shared.utils.TimeFormatter
 import com.lukasz.witkowski.training.planner.R
 import com.lukasz.witkowski.training.planner.ui.components.CategoryChip
 import com.lukasz.witkowski.training.planner.ui.components.ListCardItem
-import com.lukasz.witkowski.training.planner.ui.components.LoadingScreen
 import com.lukasz.witkowski.training.planner.ui.theme.LightDark12
-import com.lukasz.witkowski.training.planner.ui.theme.Shapes
-import timber.log.Timber
+import me.bytebeats.views.charts.line.LineChart
+import me.bytebeats.views.charts.line.LineChartData
+import me.bytebeats.views.charts.line.render.line.SolidLineDrawer
+import me.bytebeats.views.charts.line.render.point.EmptyPointDrawer
+import me.bytebeats.views.charts.line.render.xaxis.SimpleXAxisDrawer
+import me.bytebeats.views.charts.line.render.yaxis.SimpleYAxisDrawer
 
 @ExperimentalAnimationApi
 @Composable
@@ -84,7 +64,13 @@ fun TrainingOverviewScreen(
     viewModel: TrainingOverviewViewModel,
     navigateBack: () -> Unit
 ) {
-    val trainingWithExercises by viewModel.training.collectAsState(TrainingWithExercises(Training(title = ""), emptyList()))
+    val trainingWithExercises by viewModel.training.collectAsState(
+        TrainingWithExercises(
+            Training(
+                title = ""
+            ), emptyList()
+        )
+    )
     val generalStatistics by viewModel.statistics.collectAsState(emptyList())
     Scaffold(modifier = modifier) {
         LazyColumn(
@@ -107,7 +93,7 @@ fun TrainingOverviewScreen(
                 Spacer(modifier = Modifier.height(16.dp))
             }
             item {
-                if(generalStatistics.isNotEmpty()) {
+                if (generalStatistics.isNotEmpty()) {
                     TrainingStatisticsList(
                         modifier = Modifier,
                         generalStatistics = generalStatistics
@@ -135,17 +121,24 @@ fun TrainingOverviewContent(
     trainingWithExercises: TrainingWithExercises
 ) {
     val training = trainingWithExercises.training
-    Column(modifier = modifier
-        .fillMaxWidth()
-        .padding(8.dp)
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
     ) {
-        Text(modifier = Modifier.fillMaxWidth(), text = training.title, fontSize = 32.sp, textAlign = TextAlign.Center, color = MaterialTheme.colors.primary)
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = training.title,
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        if(training.description.isNotEmpty()) {
+        if (training.description.isNotEmpty()) {
             Text(text = training.description, fontSize = 18.sp)
             Spacer(modifier = Modifier.height(16.dp))
         }
-        if(trainingWithExercises.exercises.isNotEmpty()) {
+        if (trainingWithExercises.exercises.isNotEmpty()) {
             TrainingExercisesExpandableList(
                 modifier = Modifier,
                 exercises = trainingWithExercises.exercises
@@ -163,7 +156,7 @@ fun TrainingExercisesExpandableList(modifier: Modifier, exercises: List<Training
     var isExpanded by remember { mutableStateOf(false) }
 
     val angle: Float by animateFloatAsState(
-        targetValue = if(!isExpanded) 0f else 180f,
+        targetValue = if (!isExpanded) 0f else 180f,
         animationSpec = tween(durationMillis = 200, easing = LinearEasing)
     )
 
@@ -204,11 +197,16 @@ fun SingleTrainingExerciseInformation(modifier: Modifier, exercise: TrainingExer
         backgroundColor = LightDark12
     ) {
         Column() {
-            Text(text = exercise.exercise.name, fontSize = 24.sp, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Text(
+                text = exercise.exercise.name,
+                fontSize = 24.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = exercise.exercise.description, fontSize = 18.sp)
             Spacer(modifier = Modifier.height(16.dp))
-            if(exercise.exercise.category != Category.None) {
+            if (exercise.exercise.category != Category.None) {
                 CategoryChip(
                     modifier = Modifier.fillMaxWidth(),
                     text = exercise.exercise.category.name
@@ -232,7 +230,7 @@ fun SingleTrainingExerciseInformation(modifier: Modifier, exercise: TrainingExer
                     Text(text = "Sets:")
                     Text(text = exercise.sets.toString())
                 }
-                if(exercise.time > 0L) {
+                if (exercise.time > 0L) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
@@ -242,7 +240,7 @@ fun SingleTrainingExerciseInformation(modifier: Modifier, exercise: TrainingExer
                 }
             }
             Spacer(modifier = Modifier.height(16.dp))
-            if(exercise.restTime > 0L) {
+            if (exercise.restTime > 0L) {
                 Row(
                     Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
@@ -272,14 +270,19 @@ fun TrainingStatisticsList(
 }
 
 @Composable
-fun SingleTrainingStatisticsItem(modifier: Modifier = Modifier, generalStatistics: GeneralStatistics) {
+fun SingleTrainingStatisticsItem(
+    modifier: Modifier = Modifier,
+    generalStatistics: GeneralStatistics
+) {
     val time = TimeFormatter.millisToTime(generalStatistics.time)
-    val burnedCalories = if(generalStatistics.burnedCalories == 0.0) stringResource(R.string.no_data) else stringResource(
-        id = R.string.total_burned_calories, generalStatistics.burnedCalories
-    )
-    val maxHeartRateStatistics = if(generalStatistics.maxHeartRate == 0.0) stringResource(R.string.no_data) else stringResource(
-        id = R.string.max_heart_rate, generalStatistics.maxHeartRate
-    )
+    val burnedCalories =
+        if (generalStatistics.burnedCalories == 0.0) stringResource(R.string.no_data) else stringResource(
+            id = R.string.total_burned_calories, generalStatistics.burnedCalories
+        )
+    val maxHeartRateStatistics =
+        if (generalStatistics.maxHeartRate == 0.0) stringResource(R.string.no_data) else stringResource(
+            id = R.string.max_heart_rate, generalStatistics.maxHeartRate
+        )
     val fontSize = 16.sp
     ListCardItem(modifier = modifier) {
         Column(modifier = Modifier.padding(8.dp)) {
@@ -291,10 +294,15 @@ fun SingleTrainingStatisticsItem(modifier: Modifier = Modifier, generalStatistic
             )
             Text(text = stringResource(id = R.string.time_text, time), fontSize = fontSize)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = stringResource(id = R.string.burned_calories_text, burnedCalories), fontSize = fontSize)
+            Text(
+                text = stringResource(id = R.string.burned_calories_text, burnedCalories),
+                fontSize = fontSize
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = stringResource(id = R.string.max_heart_rate_text, maxHeartRateStatistics), fontSize = fontSize)
-            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(id = R.string.max_heart_rate_text, maxHeartRateStatistics),
+                fontSize = fontSize
+            )
             HeartRateLineChart(
                 modifier = Modifier,
                 data = generalStatistics.heartRateDuringTraining
@@ -309,14 +317,28 @@ fun HeartRateLineChart(
     data: List<Double>
 ) {
     val lineChartData = LineChartData(
-        points = data.map { LineChartData.Point(it.toFloat(), label = "") },
+        points = data.mapIndexed { index, heartRate -> LineChartData.Point(heartRate.toFloat(), label = "") },
         padBy = 1.0f,
         startAtZero = false
     )
-    LineChart(
-        modifier = modifier,
-        lineChartData = lineChartData
-    )
+    if(lineChartData.points.isNotEmpty()) {
+        Spacer(modifier = Modifier.height(8.dp))
+        LineChart(
+            modifier = modifier.heightIn(max = 200.dp).padding(16.dp),
+            lineChartData = lineChartData,
+            pointDrawer = EmptyPointDrawer,
+            lineDrawer = SolidLineDrawer(color = MaterialTheme.colors.primary),
+            yAxisDrawer = SimpleYAxisDrawer(
+                labelTextColor = MaterialTheme.colors.primaryVariant,
+                drawLabelEvery = 3,
+                axisLineColor = MaterialTheme.colors.primaryVariant
+            ),
+            xAxisDrawer = SimpleXAxisDrawer(
+                axisLineColor = MaterialTheme.colors.primaryVariant
+            ),
+            horizontalOffset = 0f
+        )
+    }
 }
 
 @Preview
@@ -324,7 +346,6 @@ fun HeartRateLineChart(
 fun HeartRateLineChartPreview() {
     HeartRateLineChart(data = listOf(13.0, 41.0, 124.0, 0.0, 151.0))
 }
-
 
 
 @Preview
