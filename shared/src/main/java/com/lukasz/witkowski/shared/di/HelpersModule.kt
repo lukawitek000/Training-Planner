f711975e568dfaaccc2c3e65763f3727ff61fbfe
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-14 14:53:24 +0100

    Copy content of helper objects

--- shared/src/main/java/com/lukasz/witkowski/shared/di/HelpersModule.kt
+++ shared/src/main/java/com/lukasz/witkowski/shared/di/HelpersModule.kt
@@ -5,20 +5,19 @@ import com.lukasz.witkowski.shared.currentTraining.TrainingProgressController
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ServiceComponent::class)
 object HelpersModule {
 
-    @Singleton
     @Provides
     fun provideTimerHelper(): TimerHelper {
         return TimerHelper()
     }
 
-    @Singleton
     @Provides
     fun provideTrainingProgressController(): TrainingProgressController {
         return TrainingProgressController()
