d213e9e66bdfcbec6461fcad187eed7f81757932
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-01-15 13:53:35 +0100

    Fetch trainig

--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/currentTraining/CurrentTrainingScreen.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/currentTraining/CurrentTrainingScreen.kt
@@ -1,10 +1,18 @@
 package com.lukasz.witkowski.training.planner.ui.currentTraining
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.lukasz.witkowski.shared.models.TrainingWithExercises
+import com.lukasz.witkowski.shared.utils.ResultHandler
 
 @Composable
 fun CurrentTrainingScreen(
@@ -12,8 +20,36 @@ fun CurrentTrainingScreen(
     viewModel: CurrentTrainingViewModel,
     navigateBack: () -> Unit
 ) {
+    val trainingFetchState by viewModel.trainingFetchState.collectAsState(initial = ResultHandler.Loading)
+
     Scaffold(modifier = modifier.fillMaxSize()) {
         Text(text = "Training id ${viewModel.trainingId}")
+        when(trainingFetchState) {
+            is ResultHandler.Loading -> {
+                LoadingTrainingContent()
+            }
+            is ResultHandler.Success -> {
+                CurrentTrainingContent(modifier = Modifier, trainingWithExercises = (trainingFetchState as ResultHandler.Success<TrainingWithExercises>).value)
+            }
+            else -> Unit
+        }
+
     }
 
 }
+
+@Composable
+private fun LoadingTrainingContent() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun CurrentTrainingContent(
+    modifier: Modifier,
+    trainingWithExercises: TrainingWithExercises
+) {
+    Text(text = "Training \n $trainingWithExercises")
+}
+
--- app/src/main/java/com/lukasz/witkowski/training/planner/ui/currentTraining/CurrentTrainingViewModel.kt
+++ app/src/main/java/com/lukasz/witkowski/training/planner/ui/currentTraining/CurrentTrainingViewModel.kt
@@ -2,17 +2,30 @@ package com.lukasz.witkowski.training.planner.ui.currentTraining
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.lukasz.witkowski.shared.models.TrainingWithExercises
+import com.lukasz.witkowski.shared.utils.ResultHandler
 import com.lukasz.witkowski.training.planner.repository.StatisticsRepository
+import com.lukasz.witkowski.training.planner.repository.TrainingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class CurrentTrainingViewModel @Inject constructor(
     private val statisticsRepository: StatisticsRepository,
+    private val trainingRepository: TrainingRepository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     val trainingId = savedStateHandle.get<Long>("trainingId") ?: -1
+
+    val trainingFetchState = trainingRepository.getTrainingById(trainingId).map {
+        ResultHandler.Success(it)
+    }
+
+
 
 
 }
