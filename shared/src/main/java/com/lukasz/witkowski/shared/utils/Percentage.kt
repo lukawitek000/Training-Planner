0f533279d160f94bbed867c8a153c2c84ab74f1e
Łukasz Witkowski <lukasz.witkowski000@gmail.com>
2022-04-30 21:23:42 +0200

    Improve percentage to string with tests

--- shared/src/main/java/com/lukasz/witkowski/shared/utils/Percentage.kt
+++ shared/src/main/java/com/lukasz/witkowski/shared/utils/Percentage.kt
@@ -1,6 +1,9 @@
 package com.lukasz.witkowski.shared.utils
 
+import kotlin.math.roundToInt
+
 fun Double.toPercentage(): String {
-    val number = (this * 100).toInt()
+    require(this >= 0.0) { "Number cannot be converted to percentage" }
+    val number = (this * 100).roundToInt()
     return "$number%"
 }
