7e0d9959836ea15e9263f4886f0fb75bfa2bd5aa
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-14 17:53:54 +0200

    Create custom detekt

--- image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/FileImageStorage.kt
+++ image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/FileImageStorage.kt
@@ -2,8 +2,8 @@ package com.lukasz.witkowski.training.planner.image
 
 class FileImageStorage : ImageStorage {
     override fun someMethod() {
-        val long = 2
-        val sd = 1
+//        val long = 2
+//        val sd = 1
         println("")
         println("")
         println("")
