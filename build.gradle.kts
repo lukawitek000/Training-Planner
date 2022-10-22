459c1bb17b9aa1bf0b7562885df3be593037046d
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-22 16:00:49 +0200

    app migrate to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 buildscript {
     extra.apply {
         set("kotlin_version", "1.6.10")
-        set("compose_version", "1.1.1")
     }
     repositories {
         google()
@@ -13,6 +12,8 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
 }
+
+val composeVersion by extra("1.1.1")
 
 tasks.register("clean",Delete::class){
     delete(rootProject.buildDir)
