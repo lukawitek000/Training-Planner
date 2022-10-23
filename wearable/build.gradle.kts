916da888472ecb32864d73580fc3112c09ca3638
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-23 13:22:50 +0200

    Migrate wearable to kts and minsdk 23

---
+++ wearable/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.lukasz.witkowski.training.planner"
+        minSdk = 23
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(project(":training"))
+    implementation(project(":exercise"))
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+    implementation("androidx.percentlayout:percentlayout:1.0.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.wear:wear:1.2.0")
+    implementation("androidx.wear:wear-input:1.1.0")
+
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+
+    // Hilt - dependency injection
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    kapt("com.google.dagger:hilt-compiler:2.38.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    //Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Health services
+    implementation("androidx.health:health-services-client:1.0.0-alpha03")
+    // Used to bridge between Futures and coroutines
+    implementation("androidx.concurrent:concurrent-futures-ktx:1.1.0")
+    implementation("com.google.guava:guava:30.1.1-android")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-service:2.4.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+
+    // Ongoing Activity.
+    implementation("androidx.wear:wear-ongoing:1.0.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.0")
+
+    // Gson for json serialization
+    implementation("com.google.code.gson:gson:2.8.9")
+}
