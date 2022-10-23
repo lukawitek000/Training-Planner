6e30e168803b41e2d46d059ff6780dbc4c8b561d
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-23 16:43:14 +0200

    Use buildsrc in projects build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath(BuildPlugins.android)
+        classpath(BuildPlugins.kotlin)
+        classpath(BuildPlugins.hilt)
     }
 }
 
--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -1,7 +1,7 @@
 object BuildPlugins {
     val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
     val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
-    val hilt by lazy { "com.google.dagger:hilt-android-gradle-plugin:${Versions.gradlePlugin}" }
+    val hilt by lazy { "com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}" }
 
     val commonLibraryPlugin by lazy { "common-library" }
 }
