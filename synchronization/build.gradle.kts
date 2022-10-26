5be26b646890e4a523888be6b436e344fbbd2a8c
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-26 17:35:53 +0200

    Synchronization dependencies via version catalog

--- synchronization/build.gradle.kts
+++ synchronization/build.gradle.kts
@@ -1,56 +1,11 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-}
-
-android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    id(BuildPlugins.commonLibraryPlugin)
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.4.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-    // Coroutines and Flow
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.0")
-
-    // Wearables
-    implementation("com.google.android.gms:play-services-wearable:17.1.0")
-
-    // Gson for json serialization
-    implementation("com.google.code.gson:gson:2.8.9")
-
-    //Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.coroutinesCore)
+    implementation(libs.kotlinx.coroutinesPlayServices)
+    implementation(libs.playServicesWearable)
+    implementation(libs.gson)
+    implementation(libs.timber)
 }
