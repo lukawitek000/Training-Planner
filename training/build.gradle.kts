44d8fe067794d3729892304e28543ca85ee771a8
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2022-10-26 17:08:19 +0200

    Training dependencies using version catalog

--- training/build.gradle.kts
+++ training/build.gradle.kts
@@ -1,75 +1,21 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-    kotlin("kapt")
+    id(BuildPlugins.commonLibraryPlugin)
     id("dagger.hilt.android.plugin")
-}
-
-android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 23
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+//    alias(libs.plugins.daggerAndroidPlugin)
 }
 
 dependencies {
-    implementation(project(":shared"))
     implementation(project(":exercise"))
     implementation(project(":synchronization"))
+    implementation(project(":shared"))
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-
-    // Coroutines and Flow
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.0")
-
-    // Room - database
-    val roomVersion = "2.4.2"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    kapt("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
+    implementation(libs.kotlinx.coroutinesPlayServices)
+    implementation(libs.playServicesWearable)
+    implementation(libs.timber)
+    implementation(libs.bundles.room)
+    kapt(libs.androidx.roomCompiler)
 
     // Hilt - dependency injection
-    implementation("com.google.dagger:hilt-android:2.38.1")
-    kapt("com.google.dagger:hilt-compiler:2.38.1")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
-
-    // viewmodelscope
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-
-    //Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
-    // Wearables
-    implementation("com.google.android.gms:play-services-wearable:17.1.0")
-
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-    implementation("androidx.lifecycle:lifecycle-service:2.4.1")
+    implementation(libs.dagger.hiltAndroid)
+    kapt(libs.dagger.hiltAndroidCompiler)
 }
