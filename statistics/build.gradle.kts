390eff9b40bf4624d2bdbba7b3fa67c4fdb6ecb4
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-14 20:40:09 +0100

    Order of dependencies

--- statistics/build.gradle.kts
+++ statistics/build.gradle.kts
@@ -6,9 +6,9 @@ dependencies {
     implementation(projects.training)
     api(projects.shared)
 
-    testImplementation(libs.junit)
-    testImplementation(libs.kotlinTestJunit)
-
     implementation(libs.bundles.room)
     kapt(libs.androidx.roomCompiler)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinTestJunit)
 }
