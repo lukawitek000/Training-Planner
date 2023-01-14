7e2a4852d2c89fd4c2e1ee85127731111d6d0706
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-14 21:02:45 +0100

    Improve image di

---
+++ image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/ImageContainer.kt
@@ -0,0 +1,17 @@
+package com.lukasz.witkowski.training.planner.image
+
+import android.content.Context
+import com.lukasz.witkowski.training.planner.image.domain.ChecksumCalculator
+import com.lukasz.witkowski.training.planner.image.domain.ImageReferenceRepository
+import com.lukasz.witkowski.training.planner.image.domain.ImageRepository
+import com.lukasz.witkowski.training.planner.image.infrastructure.Adler32ChecksumCalculator
+import com.lukasz.witkowski.training.planner.image.infrastructure.DbImageReferenceRepository
+import com.lukasz.witkowski.training.planner.image.infrastructure.InternalStorageImageRepository
+import com.lukasz.witkowski.training.planner.image.infrastructure.db.ImageReferenceDatabase
+
+internal class ImageContainer(context: Context, directoryName: String) {
+    private val database = ImageReferenceDatabase.getInstance(context)
+    val imageRepository: ImageRepository = InternalStorageImageRepository(context, directoryName)
+    val imageReferenceRepository: ImageReferenceRepository = DbImageReferenceRepository(database.imageReferenceDao())
+    val checksumCalculator: ChecksumCalculator = Adler32ChecksumCalculator()
+}
