9a9b70ac000698a0582a797292083c8d317d9f4b
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-15 18:30:02 +0100

    Override equals and hashcode in Image and ImageConfiguration

--- image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/ImageConfiguration.kt
+++ image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/ImageConfiguration.kt
@@ -5,4 +5,15 @@ import java.util.UUID
 data class ImageConfiguration(
     val data: ByteArray,
     val ownerId: UUID
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return (other as? ImageConfiguration)?.let {
+            data.contentEquals(it.data) && ownerId == it.ownerId
+        } ?: false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * data.contentHashCode() + ownerId.hashCode()
+    }
+}
