dad129b607744162b8f95c5c670fbb3fe6dfb4de
Łukasz Witkowski <58557988+lukawitek000@users.noreply.github.com>
2023-01-15 18:42:12 +0100

    Override equals and hashcode in Image and ImageConfiguration (#57)

--- image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/Image.kt
+++ image/src/main/kotlin/com/lukasz/witkowski/training/planner/image/Image.kt
@@ -3,4 +3,15 @@ package com.lukasz.witkowski.training.planner.image
 data class Image internal constructor(
     val imageId: ImageId,
     val data: ByteArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return (other as? Image)?.let {
+            data.contentEquals(it.data) && imageId == it.imageId
+        } ?: false
+    }
+
+    override fun hashCode(): Int {
+        return 31 * imageId.hashCode() + data.contentHashCode()
+    }
+}
