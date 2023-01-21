1c7b8d75f9c4ec63da1c39e5419dac2d88cba3fe
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-21 17:40:54 +0100

    Set up rest time fragment

--- wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/trainingSession/TrainingRestTimeFragment.kt
+++ wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/trainingSession/TrainingRestTimeFragment.kt
@@ -5,20 +5,83 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.lukasz.witkowski.training.planner.databinding.FragmentTrainingRestTimeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class TrainingRestTimeFragment : Fragment() {
 
     private lateinit var binding: FragmentTrainingRestTimeBinding
+    private val sharedViewModel by activityViewModels<TrainingSessionViewModel>()
+    private val viewModel by viewModels<TrainingExerciseViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentTrainingRestTimeBinding.inflate(layoutInflater, container, false)
+        observeTrainingExercise()
+        setUpSkipButton()
         return binding.root
+    }
+
+    private fun observeTrainingExercise() {
+        sharedViewModel.currentExercise.observe(viewLifecycleOwner) {
+            viewModel.setTimer(it.restTime)
+            viewModel.startTimer()
+            setUpTimerView()
+        }
+    }
+
+    private fun setUpTimerView() {
+        launchInStartedState {
+            observeTimer()
+            observeTimerFinished()
+        }
+    }
+
+    private suspend fun observeTimer() {
+        viewModel.timer.collectLatest {
+            Timber.d("LWWW timer ${it.toTimerString()}")
+            binding.restTimeTimerTv.text = it.toTimerString(false)
+            if(it.isZero()) {
+                Timber.d("LWWW timer is 0")
+                sharedViewModel.skip()
+            }
+        }
+    }
+
+    private fun setUpSkipButton() {
+        binding.skipRestTimeTv.setOnClickListener {
+            viewModel.stopTimer()
+            sharedViewModel.skip()
+        }
+    }
+
+    private suspend fun observeTimerFinished() {
+        viewModel.hasFinished.collectLatest {
+            Timber.d("LWWW has finished $it") // TODO never called
+            if (it) {
+                sharedViewModel.skip()
+            }
+        }
+    }
+
+    private fun launchInStartedState(block: suspend CoroutineScope.() -> Unit) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                block()
+            }
+        }
     }
 
     companion object {
