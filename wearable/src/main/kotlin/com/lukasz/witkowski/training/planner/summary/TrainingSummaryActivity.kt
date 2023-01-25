68470e91499a4d1c8c582c21627adb20f94dd1c4
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-25 19:23:34 +0100

    Improve summary view with custom view

--- wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/summary/TrainingSummaryActivity.kt
+++ wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/summary/TrainingSummaryActivity.kt
@@ -4,21 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.lukasz.witkowski.training.planner.R
 import com.lukasz.witkowski.training.planner.databinding.ActivityTrainingSummaryBinding
 import com.lukasz.witkowski.training.planner.statistics.domain.models.TrainingStatistics
 import com.lukasz.witkowski.training.planner.utils.launchInStartedState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class TrainingSummaryActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityTrainingSummaryBinding
     private val viewModel by viewModels<TrainingSummaryViewModel>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.TrainingPlannerTheme)
@@ -40,8 +37,7 @@ class TrainingSummaryActivity : ComponentActivity() {
         binding.apply {
             summaryContentLayout.visibility = View.VISIBLE
             loadingStatisticsPb.visibility = View.GONE
-            totalTimeTv.text = trainingStatistics.totalTime.toString()
-//            burnedCaloriesTv.text = trainingStatistics.id.toString()
+            totalTimeSv.setStatisticValue(trainingStatistics.totalTime.toString())
         }
     }
 }
--- wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/summary/TrainingSummaryViewModel.kt
+++ wearable/src/main/kotlin/com/lukasz/witkowski/training/planner/summary/TrainingSummaryViewModel.kt
@@ -1,20 +1,11 @@
 package com.lukasz.witkowski.training.planner.summary
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.lukasz.witkowski.shared.utils.ResultHandler
-import com.lukasz.witkowski.training.planner.startTraining.StartTrainingActivity
 import com.lukasz.witkowski.training.planner.statistics.application.TrainingStatisticsService
-import com.lukasz.witkowski.training.planner.statistics.domain.models.TrainingStatistics
 import com.lukasz.witkowski.training.planner.statistics.domain.models.TrainingStatisticsId
-import com.lukasz.witkowski.training.planner.training.domain.TrainingPlanId
 import com.lukasz.witkowski.training.planner.trainingSession.TrainingSessionActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
