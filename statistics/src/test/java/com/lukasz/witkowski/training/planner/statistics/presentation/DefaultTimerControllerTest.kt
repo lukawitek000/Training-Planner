06b2cea22be12ec6bc038dbf00af3eb193f5835d
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-01-31 20:22:00 +0100

    Initial test to timer controller

---
+++ statistics/src/test/java/com/lukasz/witkowski/training/planner/statistics/presentation/DefaultTimerControllerTest.kt
@@ -0,0 +1,15 @@
+package com.lukasz.witkowski.training.planner.statistics.presentation
+
+import com.lukasz.witkowski.shared.time.Time
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DefaultTimerControllerTest {
+
+    private val timerController = DefaultTimerController()
+
+    @Test
+    fun `initialized timer controller has the time set to 0`() {
+        assertEquals(Time.NONE, timerController.timer.value)
+    }
+}
