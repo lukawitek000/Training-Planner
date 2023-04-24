a72a69a6826fa6372e984477001ffc102e03ad6e
Lukasz Witkowski <lukasz.witkowski@tomtom.com>
2023-04-24 22:08:44 +0200

    Add ktlint

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,12 +5,14 @@ plugins {
 repositories {
     google()
     mavenCentral()
+    maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
     implementation(libs.gradlePlugin.android)
     implementation(libs.gradlePlugin.kotlin)
     implementation(libs.gradlePlugin.detekt)
+    implementation(libs.gradlePlugin.ktlint)
     // Required for the workaround to use version catalog in buildSrc https://github.com/gradle/gradle/issues/15383
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
 }
