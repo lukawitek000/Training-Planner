8548b7fa6850dbfe5c87029e0b9043e3f6b454b6
Lukasz Witkowski <lukasz.witkowski000@gmail.com>
2026-09-23 16:45:39 +0200

    Bump java version for action to work

--- buildSrc/src/main/kotlin/plugins/common-library.gradle.kts
+++ buildSrc/src/main/kotlin/plugins/common-library.gradle.kts
@@ -32,8 +32,8 @@ extensions.configure<LibraryExtension> {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
