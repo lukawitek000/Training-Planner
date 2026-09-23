5a4a3b4173d42cee80922596d827d46b76df195e
Lukasz Witkowski <lukasz.witkowski000@gmail.com>
2026-09-23 15:46:49 +0200

    Remove material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
 
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
-    implementation(libs.compose.material)
     implementation(libs.compose.material3)
     debugImplementation(libs.compose.ui.tooling)
     implementation(libs.compose.ui.tooling.preview)
